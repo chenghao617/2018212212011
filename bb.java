24c41246f8383304d42446a452fe1992eaa96aad
chenghao617 <54835601+chenghao617@users.noreply.github.com>
2019-09-10 16:35:23 +0800

    Add files via upload

--- bb.java
+++ bb.java
@@ -4,7 +4,9 @@ public class bb {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to java!");
+		System.out.println("Programming if fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Drive");
 
 	}
 
