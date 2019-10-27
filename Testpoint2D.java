356466459ab912bc2c26aa40ff84c131fde094d1
chenghao617 <54835601+chenghao617@users.noreply.github.com>
2019-10-27 17:03:29 +0800

    Add files via upload

---
+++ Testpoint2D.java
@@ -0,0 +1,21 @@
+package java;
+
+public class Testpoint2D {
+	public static void main(String[] args) {
+		TV tv1= new TV();
+		tv1.turn0n();
+		tv1.setChannel(30);
+		tv1.setVolume(3);
+		
+		TV tv2=new TV();
+		tv2.turn0n();
+		tv2.channe1Up();
+		tv2.channe1Up();
+		tv2.volumeUp();
+		
+		System.out.println("tv1's channel is "+tv1.channel+"and volume level is "+tv1.volumeLevel);
+		System.out.println("tv2's channel is "+tv2.channel+"and volume level is "+tv2.volumeLevel);
+		
+	}
+
+}
