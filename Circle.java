1b83a3c66e25721d5fe5d1ffc07099e4decef319
chenghao617 <54835601+chenghao617@users.noreply.github.com>
2019-10-29 15:45:53 +0800

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,38 +1,27 @@
+package welcome;
 
 public class Circle {
 
 	public static void main(String[] args) {
-		Circle circle1 = new Circle();
-		System.out.println("The area of the circle of radius "+ circle1.radius+" is "+circle1.getArea());
 		
-		Circle circle2 = new Circle(25);
-		System.out.println("The area of the circle of radius "+ circle2.radius+" is "+circle2.getArea());
+		double radius;
+		static int number0f0bjects = 0;
+		Circle(){
+			radius = 1;
+			number0f0bjects++;	
+		}
 		
-		Circle circle3 = new Circle(125);
-		System.out.println("The area of the circle of radius "+ circle3.radius+" is "+circle3.getArea());
+		Circle(double newRadius){
+			radius = newRadius;
+			number0f0bjects++;
+		}
 		
-		circle2.radius=100;
-		System.out.println("The area of the circle of radius "+ circle2.radius+" is "+circle2.getArea());
-	}
-
-	double radius;
-	
-	Circle(){
-		radius = 1;
-	}
-	
-	Circle(double newRadius){
-		radius=newRadius;
-	}
-	
-	double getArea(){
-		return radius*radius*Math.PI;
-	}
-	double getPerimeter(){
-		return 2*radius*Math.PI;
-	}
-	
-	void steRadius(double newRadius){
-		radius=newRadius;
+		static int getnumber0f0bjects() {
+			return number0f0bjects;
+		}
+		
+		double getArea() {
+			return radius*radius*Math.PI;
+		}
 	}
 }
