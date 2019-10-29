29aefbad2d9e3001c6878623e6e1764ebedd6337
chenghao617 <54835601+chenghao617@users.noreply.github.com>
2019-10-29 16:16:31 +0800

    Add files via upload

---
+++ TestCircleWithStaticMembers.java
@@ -0,0 +1,22 @@
+
+public class TestCircleWithStaticMembers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Before creating objects");
+		System.out.println("The number of Circle dbjets is "  + Circle.number0f0bjects);
+		
+		Circle c1= new Circle();
+		
+		System.out.println("\nAfter creating c1");
+		System.out.println("c1:radius(" +c1.radius +") and number of Circle objects("+c1.number0f0bjects + ")");
+		
+		Circle c2= new Circle(5);
+		
+		c1.radius = 9;
+		 System.out.println("\nAfter creating c2 and modifying c1");
+		 System.out.println("c1:radius(" +c1.radius +") and number of Circle objects("+c1.number0f0bjects + ")");
+		 System.out.println("c2:radius(" +c2.radius +") and number of Circle objects("+c2.number0f0bjects + ")");
+	}
+
+}
